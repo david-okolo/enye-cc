243bacdaa8a0f901cea677e5351a1ff779842108
David Okolo <david.okolo@icloud.com>
2020-06-08 17:12:04 +0100

    Updates

---
+++ precache-manifest.842cb833875e71d1ed954297c10b660d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e59df187856260016166cebe065b789",
+    "url": "/enye-cc/index.html"
+  },
+  {
+    "revision": "94b87d0f5b6ecdbf4e0c",
+    "url": "/enye-cc/static/css/main.e0b6e618.chunk.css"
+  },
+  {
+    "revision": "6348c0e3085bca2405ca",
+    "url": "/enye-cc/static/js/2.8ae31e26.chunk.js"
+  },
+  {
+    "revision": "876e403427d7be82fb4e8ac24123aaf3",
+    "url": "/enye-cc/static/js/2.8ae31e26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94b87d0f5b6ecdbf4e0c",
+    "url": "/enye-cc/static/js/main.b7eac62d.chunk.js"
+  },
+  {
+    "revision": "9369837a7dd96e4adf8e",
+    "url": "/enye-cc/static/js/runtime-main.c943a5e3.js"
+  }
+]);
