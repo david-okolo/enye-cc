0e80cfd4d3974c001230b71c86029ecd118be278
David Okolo <david.okolo@icloud.com>
2020-06-08 17:14:41 +0100

    fix(data-fetching): change backend url

--- src/lib/utils/placesRequest.ts
+++ src/lib/utils/placesRequest.ts
@@ -3,7 +3,7 @@ import { DEFAULT_SEARCH_RADIUS, DEFAULT_LAT_LNG } from "./constants";
 
 export const fetchHospitals = async (radius: number = DEFAULT_SEARCH_RADIUS, location: LatLng = DEFAULT_LAT_LNG) => {
     const radiusInMetres = radius * 1000;
-    const response = await fetch('http://localhost:4000/places/hospitals', {
+    const response = await fetch('http://52.87.235.123/places/hospitals', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
